f9c2af97933dae873bc8bbd5d82f376f9c531f8f
gabpc <grodriguezov@gmail.com>
2025-01-13 22:20:47 -0600

    fixes

--- src/app.js
+++ src/app.js
@@ -9,6 +9,7 @@ document.addEventListener('DOMContentLoaded', () => {
 addCss('/components/header.css');
 addCss('/components/footer.css');
 addFavicon();
+addFont("https://fonts.googleapis.com/css2?family=Jost:ital,wght@0,100..900;1,100..900&display=swap");
 
 function addCss(fileName) {
 
@@ -32,6 +33,23 @@ function addFavicon() {
   link.href = "/public/javascript.svg";
 
   head.appendChild(link);
+}
+
+function addFont(linkfont) {
+  let head = document.head;
+  let link1 = document.createElement("link");
+  let link2 = document.createElement("link");
+  let link3 = document.createElement("link");
+
+  link1.rel, link2.rel = "preconnect";
+  link1.href = "https://fonts.googleapis.com";
+  link2.href = "https://fonts.gstatic.com";
+  link3.href = linkfont
+  link3.rel = "stylesheet";
+
+  head.appendChild(link1);
+  head.appendChild(link2);
+  head.appendChild(link3)
 }
 
 /* Añadir la clase de active al link actual automaticamente */
