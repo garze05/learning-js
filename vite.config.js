c989c8746a3f8b73ec44b7c0cf90b4733df8f923
Gabriel <90539154+garze05@users.noreply.github.com>
2025-07-24 03:01:39 -0600

    Fixed error with Objects and Arrays path in vite.

--- vite.config.js
+++ vite.config.js
@@ -1,16 +1,23 @@
-import { resolve } from 'path'
-import { defineConfig } from 'vite';
+import { resolve } from "path";
+import { defineConfig } from "vite";
 
 export default defineConfig({
-  base: process.env.NODE_ENV === 'production' ? '/learning-js/' : '/',
-  build: {
-    rollupOptions: {
-      input: {
-        main: resolve(__dirname, 'index.html'),
-        basic: resolve(__dirname, 'src/pages/basic-exercises/index.html'),
-        manipulatingdom: resolve(__dirname, 'src/pages/manipulating-dom/index.html'),
-        todolist: resolve(__dirname, 'src/pages/todo-list/index.html'),
-      },
-    },
-  },
-})
+	base: process.env.NODE_ENV === "production" ? "/learning-js/" : "/",
+	build: {
+		rollupOptions: {
+			input: {
+				main: resolve(__dirname, "index.html"),
+				basic: resolve(__dirname, "src/pages/basic-exercises/index.html"),
+				manipulatingdom: resolve(
+					__dirname,
+					"src/pages/manipulating-dom/index.html"
+				),
+				todolist: resolve(__dirname, "src/pages/todo-list/index.html"),
+				objectsandarrays: resolve(
+					__dirname,
+					"src/pages/objetos-y-arrays/index.html"
+				),
+			},
+		},
+	},
+});
